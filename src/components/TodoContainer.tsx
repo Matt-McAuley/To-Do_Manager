47a11ec06a84a90f067be02c0b4c2cf43f1e6cb3
Matthew McAuley <mattwilliam618@gmail.com>
2024-03-28 15:05:57 -0400

    Add edit, expand, delete icons

--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -1,5 +1,10 @@
 import { Todo } from "../Classes"
 import styled from '@emotion/styled'
+import DeleteIcon from "../assets/delete.svg"
+import ExpandIcon from "../assets/dots-horizontal.svg"
+import EditIcon from "../assets/note-edit.svg"
+import { useContext } from "react"
+import { TodoListContext, TodoListContextType } from "../TodoListContext"
 
 const Container = styled.div`
     display:flex;
@@ -9,19 +14,41 @@ const Container = styled.div`
     border: 2px solid black;
 `;
 
+const Image = styled.img`
+    width: 30px;
+    cursor: pointer;
+    &:hover {
+        background-color: red;
+    }
+    &:active {
+        background-color: blue;
+    }
+`
+
 type Props = {
     todo: Todo;
 }
 
 const TodoContainer = (props: Props) => {
     const todo = props.todo;
+    const { currentProject, allTodos } = useContext(TodoListContext) as TodoListContextType;
 
-    return (
+    return currentProject.equals(allTodos) ? (
         <Container>
             <div>{todo.title}</div>
             <div>{todo.description}</div>
             <div>{todo.dueDate.toString()}</div>
             <div>{todo.priority}</div>
+        </Container>
+    ) : (
+        <Container>
+            <div>{todo.title}</div>
+            <div>{todo.description}</div>
+            <div>{todo.dueDate.toString()}</div>
+            <div>{todo.priority}</div>
+            <Image src={ExpandIcon}/>
+            <Image src={EditIcon}/>
+            <Image src={DeleteIcon}/>
         </Container>
     );
 }
