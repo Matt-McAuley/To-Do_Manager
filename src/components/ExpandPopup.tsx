0965be06777e0908dc8de8876e88b6f3d3a47a16
Matthew McAuley <mattwilliam618@gmail.com>
2024-04-07 17:54:19 -0400

    Style expand popup

--- src/components/ExpandPopup.tsx
+++ src/components/ExpandPopup.tsx
@@ -3,14 +3,64 @@ import { TodoListContext, TodoListContextType } from '../TodoListContext';
 import { useContext } from 'react';
 
 const Container = styled.div`
-  width: 70%;
-  height 70%;
+  width: 60%;
+  height: 60%;
   position: absolute;
   background-color:white;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  font-family: "DM Sans";
+  font-size: 25px;
+  display: grid;
+  grid-template-rows: 80% 20%;
+  grid-template-columns: 100%;
 `;
+
+const Info = styled.div`
+  display: grid;
+  grid-template-rows: 30% 70%;
+  grid-template-columns: 34% 33% 33%;
+  grid-row: 1/2;
+  grid-column: 1/2;
+`
+
+const Title = styled.div`
+  grid-row: 1/2;
+  grid-column: 1/2;
+  border: 2px solid black;
+  padding: 5px;
+`
+
+const Description = styled.div`
+  grid-row: 2/3;
+  grid-column: 1/4;
+  border: 2px solid black;
+  padding: 5px;
+`
+
+const Priority = styled.div`
+  grid-row: 1/2;
+  grid-column: 3/4;
+  border: 2px solid black;
+  padding: 5px;
+`
+
+const Date = styled.div`
+  grid-row: 1/2;
+  grid-column: 2/3;
+  border: 2px solid black;
+  padding: 5px;
+`
+
+const Button = styled.button`
+  grid-row: 2/3;
+  grid-column: 1/2;
+  font-family: "DM Sans";
+  font-size: 30px;
+  font-weight: bold;
+  background-color: lightgray;
+`
 
 const ExpandPopup = () => {
 
@@ -18,11 +68,13 @@ const ExpandPopup = () => {
 
     return expandPopup ? (
         <Container>
-            <div>{editInfo.todoTitle}</div>
-            <div>{editInfo.description}</div>
-            <div>{editInfo.priority}</div>
-            <div>{editInfo.date}</div>
-            <button onClick={() => {
+            <Info>
+            <Title>{editInfo.todoTitle}</Title>
+            <Description>{editInfo.description}</Description>
+            <Priority>{editInfo.priority}</Priority>
+            <Date>{editInfo.date}</Date>
+            </Info>
+            <Button onClick={() => {
                 setExpandPopup(false)
                 setEditInfo({
                     todoTitle: "",
@@ -31,7 +83,7 @@ const ExpandPopup = () => {
                     priority: "",
                     description: "",
                 })
-                }}>OK</button>
+                }}>OK</Button>
         </Container> 
     ) : null;
 
