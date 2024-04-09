3ff545e33f52ae6bbd6f22d10818322fb9753094
Matthew McAuley <mattwilliam618@gmail.com>
2024-04-08 23:02:04 -0400

    Add project popup styling

--- src/components/ProjectPopup.tsx
+++ src/components/ProjectPopup.tsx
@@ -1,17 +1,40 @@
 import styled from '@emotion/styled'
-import { TextField} from '@mui/material'
 import { TodoListContext, TodoListContextType } from '../TodoListContext';
 import { useContext } from 'react';
 
 const Container = styled.form`
-  width: 70%;
-  height 70%;
-  position: absolute;
-  background-color: white;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+    width: 35%;
+    height: 20%;
+    position: absolute;
+    background-color:white;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 8px;
+    border: 2px solid black;
+    display: grid;
+    grid-template-rows: 50% 50%;
+    grid-template-columns: 100%;
+    font-family: "DM Sans";
 `;
+
+const Title = styled.input`
+  grid-row: 1/2;
+  font-family: "DM Sans";
+  grid-column: 1/2;
+  font-size: 22px;
+  padding: 10px;
+`
+
+const Button = styled.button`
+  grid-row: 2/3;
+  grid-column: 1/2;
+  font-family: "DM Sans";
+  font-size: 30px;
+  font-weight: bold;
+  background-color: lightgray;
+  cursor: pointer;
+`
 
 const ProjectPopup = () => {
 
@@ -24,9 +47,9 @@ const ProjectPopup = () => {
             setEditInfo({...editInfo, projectTitle : ""});
             setProjectPopup(false);
             }}>
-            <TextField id="outlined-basic" label="Title" variant="outlined" placeholder='Title' 
+            <Title id="outlined-basic" placeholder='Title' 
             onChange={(evt) => setEditInfo({...editInfo, projectTitle: evt.target.value})} value={editInfo.projectTitle} required/>
-            <button type="submit">Submit</button>
+            <Button type="submit">Submit</Button>
         </Container>
     ) : null;
 
